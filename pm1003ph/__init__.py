cb0fd7fc6b10a174004440eb1d70e0a8cd9e9cae
Lukas Kolbe <lukas@einfachkaffee.de>
2025-04-06 13:48:54 +0200

    remove old files

--- pm1003ph/__init__.py
+++
@@ -1,5 +0,0 @@
-from esphome import automation
-from esphome.components import binary_sensor
-from esphome.const import CONF_ID
-
-DEPENDENCIES = ["binary_sensor"]  # Add binary_sensor as a dependency
