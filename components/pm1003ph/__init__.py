fa8d0983e5c2ccbfea26466675c3c9e2be7e0fa0
Lukas Kolbe <lukas@einfachkaffee.de>
2025-04-06 13:45:43 +0200

    add components folder for esphome compatibility

---
+++ components/pm1003ph/__init__.py
@@ -0,0 +1,5 @@
+from esphome import automation
+from esphome.components import binary_sensor
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["binary_sensor"]  # Add binary_sensor as a dependency
